14787e81868959a8cade34687e23cc0a86f0cfdf
George Maina <george.maina@finova.tech>
2022-07-25 11:40:48 +0200

    added example web service

---
+++ SOAP.WebServiceExample/Service.asmx.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using System.Web.Services;
+
+namespace SOAP.WebServiceExample
+{
+    /// <summary>
+    /// Summary description for Service
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    public class Service : WebService
+    {
+        private double GetMinInternal(params double[] numbers)
+        {
+            return numbers.Min();
+        }
+
+        [WebMethod]
+        public double GetMin(double[] d)
+        {
+            return GetMinInternal(d);
+        }
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public double GetMin3(double d1, double d2, double d3)
+        {
+            return GetMinInternal(d1, d2, d3);
+        }
+
+
+    }
+}
